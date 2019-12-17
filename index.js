494d11a285593abd440b3db6e32955ef7d2fd4f5
Marcelo Hossomi <mhossomi@bandwidth.com>
2019-12-17 18:08:14 -0200

    Add /time.

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ const catapult = require('./src/catapult')
 const bxml = require('./src/bxml')
 
 catapult('Catapult App', process.env.PORT || 8111, app => app
+    .use("/time", (req, res) => {
+        res.send(`
+        <?xml version="1.0" ?>
+        <Response>
+            <SpeakSentence gender="female" voice="susan" locale="en_US">
+                The current time is: ${new Date()}
+            </SpeakSentence>
+        </Response>`)
+    })
     .use((req, res) => res.sendStatus(200)))
